406df4a3af3e95bcef3183b05775aa511cd5504f
Nobleraj <nobleraj.raj@gmail.com>
2022-04-26 13:27:36 +0530

    1 file modified

--- Google.js
+++ Google.js
@@ -42,3 +42,56 @@ function createTable(){
   }
  }
 `;
+//create the progress bar using css and javascript
+`<!DOCTYPE html>
+<html lang="en">
+ <title>React App</title>
+  </head>
+  <body>
+    <div class="bar-container">
+    <div class="bar"></div>
+    </div>
+    <button onclick="createBar(2,true)">Click</button>
+    <style>
+     body{
+       background-color : lightgray;
+     }
+     .bar-container{
+         width: 300px;
+         height: 40px;
+         margin: 20px;
+         background-color: gray;
+     }
+     .bar{
+         background-color: green;
+         height: 100%;
+         width: 0%;
+     }
+    </style>
+    <script>
+     //args are secs and boolean flag - the progress bar by javascript - true, css - false   
+     function createBar(sec,javascript_css){
+
+        
+        let elem = document.querySelector(".bar");
+        ////using javascript
+        if(javascript_css){
+        let percentage = 0;
+        let interval = setInterval(()=>{
+            percentage++;
+            elem.style.width = percentage + '%';
+            if(percentage >= 100){
+                clearInterval(interval);
+            }
+        },(sec * 1000) /100);
+       }
+       //using css
+       if(!javascript_css){
+        elem.style.transition = `${sec}s linear width`;   
+        elem.style.width = '100%';
+       }
+     }
+    </script>
+    </body>
+</html>
+`
