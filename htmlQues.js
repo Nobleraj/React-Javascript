524747e5446ecb11bdfe458412cf7b23ce96eb68
Nobleraj <nobleraj.raj@gmail.com>
2022-05-25 16:21:16 +0530

    1 file added

---
+++ htmlQues.js
@@ -0,0 +1,6 @@
+//html vs html5
+//html5 - we are using doctype
+  //doctype means semantic tags like header, footer, section,main,article can be used
+
+  //strong vs bold tag
+  //strong tag semantically emphasizes on the important word or section of words while the bold tag is just offset text conventionally styled in bold
