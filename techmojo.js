5ec428d14550af548204b5f44af1d75a0edf7898
Nobleraj <nobleraj.raj@gmail.com>
2022-07-27 09:10:53 +0530

    1 file modified

--- techmojo.js
+++ techmojo.js
@@ -14,3 +14,19 @@ let company = {
     internals: [{ name: 'Jack', salary: 1300 }],
   },
 };
+
+function dynamicObj(src) {
+  let result = 0;
+  let keys = Object.keys(src);
+  for (var i = 0; i < keys.length; i++) {
+    let innerObj = src[keys[i]];
+    if (Array.isArray(innerObj)) {
+      for (var j = 0; j < innerObj.length; j++) {
+        result += innerObj[j].salary;
+      }
+    } else {
+      result += dynamicObj(innerObj);
+    }
+  }
+  return result;
+}
