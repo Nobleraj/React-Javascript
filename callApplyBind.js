93ecb08a9ed5b1ef9a2c1f72936eed9986c26148
Nobleraj <nobleraj.raj@gmail.com>
2022-08-02 19:26:55 +0530

    1 file modified

--- callApplyBind.js
+++ callApplyBind.js
@@ -22,3 +22,34 @@ console.log('apply', person.hello.apply(Noble, 'Good to go'));
 const hello = person.hello.bind(Noble);
 
 console.log('bind', hello('Demo'));
+
+//oputput based questions
+
+//Append an array to another array
+const array = [1, 2, 3];
+const elem = ['a', 'b'];
+
+console.log('append', array.push.apply(array, elem));
+
+//1.
+const person = { name: 'Coder' };
+
+function hello(age) {
+  return `${this.name} is age: ${age}`;
+}
+
+console.log('call', hello.call(person, 24)); //it executes the function
+console.log('bind', hello.bind(person, 24)); //it return new function
+
+//2.
+const age = 10;
+var person = {
+  name: 'Noble',
+  age: 25,
+  getAge: function () {
+    return this.age;
+  },
+};
+var person2 = { age: 29 };
+console.log('output ', person.getAge.call(person2)); // 29
+console.log('output ', person.getAge()); //25
