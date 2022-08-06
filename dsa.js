cc02fc1a03a3561ecf6549d76081245f38611eb8
Nobleraj <nobleraj.raj@gmail.com>
2022-08-06 14:35:16 +0530

    1 file modified

--- dsa.js
+++ dsa.js
@@ -27,3 +27,20 @@ function zeroAtEnd(array) {
   return array;
 }
 console.log('ZeroAtEnd ', zeroAtEnd([0, 0, 1, 2, 3, 0, 2]));
+
+//Anagram
+function anagram(str1, str2) {
+  if (str1.length != str2.length) return false;
+
+  let array = Array(256).fill(0);
+  for (var j = 0; j < str1.length; j++) {
+    array[str1.charCodeAt(j)]++;
+    array[str2.charCodeAt(j)]--;
+  }
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== 0) return false;
+  }
+
+  return true;
+}
+console.log('Is Anagram', anagram('abcde', 'edbac'));
