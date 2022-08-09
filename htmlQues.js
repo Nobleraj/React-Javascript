86805e1fb2d9640a40ee579108854414ed7f7389
Nobleraj <nobleraj.raj@gmail.com>
2022-08-10 00:05:51 +0530

    2 files modified

--- htmlQues.js
+++ htmlQues.js
@@ -4,9 +4,3 @@
 
 //strong vs bold tag
 //strong tag semantically emphasizes on the important word or section of words while the bold tag is just offset text conventionally styled in bold
-
-//Render to string - it is basically used in server side rendering. In SSR, what happens our html is generated over the server and send to the client.This function(ReactDOMServer.renderToString()) is used to tranfer to send that html as string to the client
-ReactDOMServer.renderToString();
-
-ReactDOM.hydrate();
-//- when you receive the generated html from the server,ReactDOM.hydrate i sused to attached event handlers and make the page responsive
--- infiniteCurrying.js
+++ infiniteCurrying.js
@@ -1,3 +1,9 @@
+//Render to string - it is basically used in server side rendering. In SSR, what happens our html is generated over the server and send to the client.This function(ReactDOMServer.renderToString()) is used to tranfer to send that html as string to the client
+ReactDOMServer.renderToString();
+
+ReactDOM.hydrate();
+//- when you receive the generated html from the server,ReactDOM.hydrate i sused to attached event handlers and make the page responsive
+
 function infiniteCurring(a) {
   return (b) => {
     if (b) {
