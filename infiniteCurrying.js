236df68907f7fc010d5def4caa8d956b70feb32c
Nobleraj <nobleraj.raj@gmail.com>
2022-08-10 23:22:54 +0530

    1 file modified

--- infiniteCurrying.js
+++ infiniteCurrying.js
@@ -4,6 +4,28 @@ ReactDOMServer.renderToString();
 ReactDOM.hydrate();
 //- when you receive the generated html from the server,ReactDOM.hydrate i sused to attached event handlers and make the page responsive
 
+//Position Property
+  --Fixed, Sticky, Relative, Absolute, Static;
+//Symmetrical layout(with flex, grid), Asymitrical layout (with position property)
+
+//Box- Sizing
+content-box, border-box
+
+//== vs ===
+//loose equality(type conversion) vs strict equality(runs faster)
+
+DOMContentLoaded vs onload
+//onLoad fires once all the resources are downloaded like img,ads and css, basically it waits for all the resorses to downlaod and it fires
+//DOMContentLoaded fires before all the resources gets downloaded,basicallt it won't wait
+
+//Element.getBoundingClientRect()
+
+//AddBLocker - is plugin that blocks the request that downloads the content
+
+//What happens when we type url in browser and Enter
+
+//Pagespeed insights - FCP, LCP
+
 function infiniteCurring(a) {
   return (b) => {
     if (b) {
