1694db6bfdd42d535c4541dda9cf79b4f0ed8263
Nobleraj <nobleraj.raj@gmail.com>
2022-08-14 23:46:24 +0530

    1 file modified

--- utils.js
+++ utils.js
@@ -1,3 +1,46 @@
+//Least recently used cache
+class LRU {
+  constructor(max = 5) {
+    this.max = max;
+    this.cache = new Map();
+  }
+  set_(key, value) {
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
+    } else if (this.max == this.cache.size) {
+      this.cache.delete(this.findFirst());
+    }
+    this.cache.set(key, value);
+  }
+  findFirst() {
+    return this.cache.keys().next().value;
+  }
+  get_(key) {
+    let itm = this.cache.get(key);
+    this.cache.delete(key);
+    this.set_(key, itm);
+  }
+  getSample() {
+    return this.cache;
+  }
+}
+
+let LC = new LRU(3);
+LC.set_('1', 1);
+LC.set_('2', 1);
+LC.set_('3', 11);
+console.log('LC', LC.getSample());
+LC.set_('4', 11);
+console.log('LC', LC.getSample());
+LC.set_('2', 11);
+console.log('LC', LC.getSample());
+LC.set_('5', 11);
+console.log('LC', LC.getSample());
+LC.set_('4', 12);
+console.log('LC', LC.getSample());
+LC.set_('3', 12);
+console.log('LC', LC.getSample());
+
 //Convert 24hrs format
 const conver12to24 = (data) => {
   //02:24 PM
