63da170c0532a55077eb79c1fc71379ad2a6c5d8
Nobleraj <nobleraj.raj@gmail.com>
2022-08-18 14:08:54 +0530

    1 file modified

--- jio.js
+++ jio.js
@@ -1,6 +1,52 @@
 //Intersection of linked list
 
 //Increament counter in javascript class, closure
+//using class
+class Timer {
+  constructor(timer) {
+    this.timer = timer;
+  }
+  increase() {
+    this.timer++;
+    console.log('increase', this.timer);
+  }
+  decrease() {
+    this.timer--;
+    console.log('decrease', this.timer);
+  }
+  getTime() {
+    return this.timer;
+  }
+}
+let timer = new Timer(10);
+timer.increase();
+timer.increase();
+timer.decrease();
+timer.increase();
+timer.increase();
+console.log('result', timer.getTime());
+
+//using functions with closure
+//strictly get the function in another variable and call the inner method then only closure value gets updated in scope else it won't get updated
+function closure() {
+  let timer = 10;
+  function increase() {
+    timer++;
+    console.log('increase', timer);
+  }
+  function decrease() {
+    timer--;
+    console.log('decrease', timer);
+  }
+  return { increase, decrease };
+}
+let temp = closure();
+temp.increase();
+temp.increase();
+temp.increase();
+temp.decrease();
+temp.decrease();
+temp.increase();
 
 //Navigation restriction for user - useHistory.push
 //push same route multiple times
