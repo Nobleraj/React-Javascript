313565ef3ebcc6d831f970922aa51d2c64937470
Nobleraj <nobleraj.raj@gmail.com>
2022-08-26 12:56:37 +0530

    1 file modified

--- LinkedList&Stack.js
+++ LinkedList&Stack.js
@@ -2,22 +2,22 @@
 
 /*
 Reverse Linked List - curr, prev, nxt
-Delete Middle
-Find Middle, 
+Add Two linked list - [9->1] [9] ans=> [0->2] - carry 
+Delete Middle -  fast , slow & pre 
+Find Middle - - fast, slow
 Reverse List II - with M & N - find prev & curr from M, M - 1 
-Find Loop in LinkedList
+Find Loop in LinkedList - Floy's cycle detection algo
 Remove n from Last - LinkedList
-Sort Linked List
-Remove Duplicates in LinkedList
+Sort Linked List - use merge sort
+Remove Duplicates in LinkedList - fast, slow, base condition if prev not null, prev.next = null
 Reorder LinkedList - A0,An-1,A1,An-2....An
-Intersection LinkedList
+Intersection LinkedList - difference of A & B
 Merge K sorted List - ?
 Clone a Linked List - ?
 
 */
 
 //Stack
-
 /*
 Double Character Trouble - [3,1,1,3,4]
 Sort Stack using another stack - Until stack 1 empty, just pop and push values to another stack untill its sorted
