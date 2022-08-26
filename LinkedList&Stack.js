689fdf872d551c26aae4e964216b3acf27991a41
Nobleraj <nobleraj.raj@gmail.com>
2022-08-26 12:52:49 +0530

    1 file added

---
+++ LinkedList&Stack.js
@@ -0,0 +1,26 @@
+//Linked List
+
+/*
+Reverse Linked List - curr, prev, nxt
+Delete Middle
+Find Middle, 
+Reverse List II - with M & N - find prev & curr from M, M - 1 
+Find Loop in LinkedList
+Remove n from Last - LinkedList
+Sort Linked List
+Remove Duplicates in LinkedList
+Reorder LinkedList - A0,An-1,A1,An-2....An
+Intersection LinkedList
+Merge K sorted List - ?
+Clone a Linked List - ?
+
+*/
+
+//Stack
+
+/*
+Double Character Trouble - [3,1,1,3,4]
+Sort Stack using another stack - Until stack 1 empty, just pop and push values to another stack untill its sorted
+Min stack - push min to another stack and maintain
+Max Frequency stack - dynamic 
+*/
