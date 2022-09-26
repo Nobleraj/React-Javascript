e28320a56079d767db60fb09b4c9e56b891aee44
Nobleraj <32593373+Nobleraj@users.noreply.github.com>
2022-09-26 11:54:45 +0530

    1 file modified

--- cachingFunction.js
+++ cachingFunction.js
@@ -1,24 +1,31 @@
-function memorize(fn){
+function memorize(fn) {
   let res = {};
-  return function(...args){
+  return function (...args) {
     var argsCache = JSON.stringify(args);
-    if(!res[argsCache]){
-      res[argsCache] = fn.call(this,...args);
+    if (!res[argsCache]) {
+      res[argsCache] = fn.call(this, ...args);
     }
     return res[argsCache];
-  }  
+  };
 }
 
-const catching =(num1,num2)=>{
- for(var i=1;i<=100000000;i++){}
- return num1 * num2;;
-}
+const catching = (num1, num2) => {
+  for (var i = 1; i <= 100000000; i++) {}
+  return num1 * num2;
+};
 const M = memorize(catching);
 
-console.time("function call");
-console.log(M(3,4));
-console.timeEnd("function call");
+console.time('function call');
+console.log(M(3, 4));
+console.timeEnd('function call');
 
-console.time("S function call");
-console.log(M(3,4));
-console.timeEnd("S function call");
+console.time('S function call');
+console.log(M(3, 4));
+console.timeEnd('S function call');
+
+//Memorize factorial and faibanocci series
+const fac = memorize((x) => {
+  if (x == 1 || x == 0) return x;
+  return fac(x - 1) + fac(x - 2);
+});
+console.log('output 5', fac(5));
