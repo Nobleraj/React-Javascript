d44907db489b2549c99124c0acd34e7a54e566c0
Nobleraj <32593373+Nobleraj@users.noreply.github.com>
2022-10-12 23:31:52 +0530

    1 file added

---
+++ Redux.js
@@ -0,0 +1,3 @@
+//Redux Middleware allows you to intercept every action sent to the reducer so you can make changes to the action or cancel the action.
+
+//Middleware helps you with logging, error reporting, making asynchronous requests, and a whole lot more.
