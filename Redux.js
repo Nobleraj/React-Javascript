42cd3edf79741a06aeca72271450e284e9acd055
Nobleraj <32593373+Nobleraj@users.noreply.github.com>
2022-10-13 11:03:39 +0530

    1 file modified

--- Redux.js
+++ Redux.js
@@ -1,3 +1,5 @@
 //Redux Middleware allows you to intercept every action sent to the reducer so you can make changes to the action or cancel the action.
 
 //Middleware helps you with logging, error reporting, making asynchronous requests, and a whole lot more.
+
+//Redux Saga. In thunk, action creator does not return an object, it returns a function, In the saga, it allows you to send action normally. but it has a watcher. whenever a particular action gets dispatched, the watcher catches it.
