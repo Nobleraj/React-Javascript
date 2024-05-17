df6d91ada3ec32ae589bd0ea4fda3bcfd23378e7
Nobleraj <32593373+Nobleraj@users.noreply.github.com>
2024-05-17 15:10:48 +0530

    1 file modified

--- AMEX.js
+++ AMEX.js
@@ -8,3 +8,13 @@
 //8. Two way data binding, it's pros and cons
 //9. Graphql, pros and cons compared to the rest API.
 //10. n+1 problem of graphql
+
+//What is TDD and advantages of TDD?
+
+//Mutation testing and its use cases?
+//Mutation testing is designed to ensure the quality of a software testing tool
+//The goal is to ensure a software test can detect code that isn't properly tested or hidden defects that other testing methods don't catch
+//Use cases
+//Statement mutation. Statements are deleted or replaced with a different statement. For example, the statement "A=10 by B=5" is replaced with "A=5 by B=15."
+//Value mutation. Values are changed to find errors. For example, "A= 15" is changed to "A= 10" or "A=25."
+//Decision mutation. Arithmetic or logical operators are changed to detect errors. For example, "(A<B)" is changed to "(A>B)."
