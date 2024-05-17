529ea172ebca39cbd8839015ee1b91f2d80010b7
Nobleraj <32593373+Nobleraj@users.noreply.github.com>
2024-05-17 14:36:19 +0530

    1 file modified

--- AMEX.js
+++ AMEX.js
@@ -1,10 +1,10 @@
 //1. Roles and responsibilities in the previous project.
-2. How do you handle conflicts?
-3. Workflow of how to handle a feature development using Jira?
-4. What is TDD and advantages of TDD?
-5. Mutation testing and its use cases?
-6. Virtual Dom
-7. Handling forms in React vs Angular
-8. Two way data binding, it's pros and cons
-9. Graphql, pros and cons compared to the rest API.
-10. n+1 problem of graphql
+//2. How do you handle conflicts?
+//3. Workflow of how to handle a feature development using Jira?
+//4. What is TDD and advantages of TDD?
+//5. Mutation testing and its use cases?
+//6. Virtual Dom
+//7. Handling forms in React vs Angular
+//8. Two way data binding, it's pros and cons
+//9. Graphql, pros and cons compared to the rest API.
+//10. n+1 problem of graphql
