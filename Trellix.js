56f66bf9683901cc41e95e8fd4ac4864e12ce6d1
Nobleraj <git@stackblitz.com>
2025-07-02 17:05:48 +0530

    1 file added

---
+++ Trellix.js
@@ -0,0 +1,11 @@
+// About v8 engine
+
+//third largest
+
+//compile or intpretor language
+
+//everything is object in js but why number. giving properties
+//let number1 = new Number(6); giving object
+
+//how micro front end works in front end - any algo like federated renderin
+//how app will be rendered in another app
