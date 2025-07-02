53d5b3a365e723715b434dd21c5c780d27e449f1
Nobleraj <git@stackblitz.com>
2025-07-02 21:30:54 +0530

    1 file modified

--- Trellix.js
+++ Trellix.js
@@ -4,10 +4,29 @@
 
 //compile or intpretor language
 
-//everything is object in js but why number. giving properties
+//everything is object in javascript but why number. giving properties
 //let number1 = new Number(6); giving object
 
 //how micro front end works in front end - any algo like federated renderin
 //how app will be rendered in another app
 
+//Module Federation (Webpack 5)
+
 //performance optimization in web application
+
+//JRE - Javascript Runtime Environment
+
+//JS Engine -> Parsing -> Compilation -> Execution
+
+//Parsing -> parse the code
+// Syntax parser -> converts to AST -> Abstarct syntax tree
+// send the AST high level code to compilation phase
+// compilation and Execution happens hand in hand
+
+// Interpretor & Compiler
+// Interpretor -> Fast, executes line by line
+// Compiler -> Optimize the code and compile all the code before executing
+// Then comiler send buye code to Execution phase
+// In the mean time, Compiler would optimize the code in runtime (that's y it's JIT)
+
+// jit  - Just intime compilation
